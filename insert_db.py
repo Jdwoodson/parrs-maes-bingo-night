e6d3ab85f0c8810539faf063e5d52247da97f0dd
Justin Woodson <jdwoodson@gmail.com>
2022-10-25 22:19:56 +0000

    include firstname lastname payment status

--- insert_db.py
+++ insert_db.py
@@ -5,18 +5,20 @@ db_file = sys.argv[1]
 
 def input_data():
     form_id = int(input('Form ID#: '))
-    full_name = str(input('First and Last name: '))
+    first_name = str(input('First names: '))
+    last_name = str(input('Last name: '))
     adults = int(input('Number of adults: '))
     children = int(input('Number of children: '))
     cards = int(input('Number of bingo cards: '))
     daubers = int(input('Number of daubers: '))
     chicken = int(input('Number of sandwhiches: '))
-    data = (form_id, full_name, adults, children, cards, daubers, chicken)
+    paid = str(input('Full Prepayment?: '))
+    data = (form_id, last_name, first_name, adults, children, cards, daubers, chicken, paid)
     with sqlite3.connect(db_file) as conn:
         print('Created the connection!')
         conn.execute("""
-                insert into form_return(form_id, full_name, adults, children, cards, daubers, chicken)
-                values(?,?,?,?,?,?,?)
+                insert into form_return(form_id, last_name, first_name, adults, children, cards, daubers, chicken, paid)
+                values(?,?,?,?,?,?,?,?)
                 """, data)
         print('Inserted values into the table!')
         
--- read_db.py
+++ read_db.py
@@ -9,11 +9,12 @@ with sqlite3.connect(db_file) as conn:
                    select * from form_return
                    """)
     for row in cursor.fetchall():
-        form_id, full_name, adults, children, cards, daubers, chicken = row
-        print("{}:\n"
+        form_id, last_name, first_name, adults, children, cards, daubers, chicken = row
+        print("{} {}:\n"
               "------------ {}\n"
               "Adults:           {}\n"
               "Children:         {}\n"
               "Num of Cards:     {}\n"
               "Num of Daubers:   {}\n"
-              "Num of Chicken:   {}\n\n".format(full_name,form_id, adults, children, cards, daubers, chicken))
+              "Num of Chicken:   {}\n"
+              "Paid status:      {}\n\n".format(first_name, last_name, form_id, adults, children, cards, daubers, chicken))
